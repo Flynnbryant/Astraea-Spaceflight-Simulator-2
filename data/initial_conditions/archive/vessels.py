55967120e31b63c4aa076e1ccd049817ca2a2510
Flynnbryant <flynn.bryant2001@gmail.com>
2022-07-20 22:48:45 +1000

    Updated and organised input data and initial conditions.

---
+++ data/initial_conditions/archive/vessels.py
@@ -0,0 +1,1 @@
+Spacecraft     ,vessel ,Saturn     ,body   ,"255,255,255"   ,elements   ,0.01        ,0.01        ,0.01           ,1.00000000E+00 ,None       ,None       ,None       ,None           ,None   ,none
