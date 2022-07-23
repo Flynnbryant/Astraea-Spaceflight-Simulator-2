Now using new higher precision input data and spacecraft TLEs. Fixed Pluto system rotational axes.

data/universe.py
@@ -24,13 +24,17 @@ class Universe:
         self.vessels = []
         self.entities = []
 
-        with open(f'data/initial_conditions/archive/full.csv') as f:
-            data = list(csv.reader(f))
-            for entity in data[1:]:
-                Body(entity, self, self.focusinput) if entity[1] == 'body' else Vessel(entity, self, self.focusinput)
-            #initialise_entities()
-            #configure_masses()
-            #generate_orbits()
+        with open('data/initial_conditions/bodies.csv') as file:
+            data = list(csv.reader(file))
+            for body in data[1:]:
+                body = [item.strip() for item in body]
+                Body(body, self, self.focusinput)
+
+        with open('data/initial_conditions/vessels.csv') as file:
+            data = list(csv.reader(file))
+            for vessel in data[1:]:
+                vessel = [item.strip() for item in vessel]
+                Vessel(vessel, self, self.focusinput)
 
         self.entitylength = len(self.entities)
         self.bodylength = len(self.bodies)-1
@@ -43,3 +47,45 @@ class Universe:
             body.satellites = body.bodycentre.satellites + body.barycentre.satellites
             body.barycentre_transition = 0.5*body.barycentre.satellites[0].orbit.semi_major_axis if body.barycentre.satellites else 0.
             body.orbital_environment(self)
+
+    def read_initial_elements(self, entity):
+        with open(f'data/initial_conditions/horizons_elements/{entity.name}.txt') as file:
+            flag = False
+            for line in file:
+                if flag:
+                    elements = line.split(',')
+                    break
+                elif '$$SOE' in line:
+                    flag = True
+
+        entity.orbit.semi_major_axis    = 1000*sn(elements[11])
+        entity.orbit.eccentricity       = sn(elements[2])
+        entity.orbit.inclination        = np.deg2rad(sn(elements[4]))
+        entity.orbit.arg_periapsis      = np.deg2rad(sn(elements[6]))
+        entity.orbit.long_ascending     = np.deg2rad(sn(elements[5]))
+        entity.orbit.mean_anomaly       = np.deg2rad(sn(elements[9]))
+
+        rotation_constants(entity.orbit)
+        entity.bodycentre.rpos = entity.orbit.elliptical_elements_to_pos(entity.orbit.mean_anomaly)
+        entity.bodycentre.rvel = entity.orbit.elliptical_elements_to_vel()
+        entity.orbit.state_to_elements(entity.bodycentre, self.time)
+
+    def read_initial_TLE(self, entity):
+        with open(f'data/initial_conditions/spacecraft_TLE/{entity.name}.txt') as file:
+            linelist = [line for line in file]
+
+        entity.orbit.semi_major_axis    = np.cbrt(entity.primary.SGP/(float(linelist[2][52:63])*7.2722052166431e-5)**2)
+        entity.orbit.eccentricity       = float('0.'+linelist[2][26:33])
+        entity.orbit.inclination        = np.deg2rad(float(linelist[2][8:16]))
+        entity.orbit.arg_periapsis      = np.deg2rad(float(linelist[2][34:42]))
+        entity.orbit.long_ascending     = np.deg2rad(float(linelist[2][17:25]))
+        entity.orbit.mean_anomaly       = np.deg2rad(float(linelist[2][43:51]))
+
+        rotation_constants(entity.orbit)
+        entity.bodycentre.rpos = entity.orbit.elliptical_elements_to_pos(entity.orbit.mean_anomaly)
+        entity.bodycentre.rvel = entity.orbit.elliptical_elements_to_vel()
+        entity.orbit.state_to_elements(entity.bodycentre, self.time)
+
+def sn(datastr):
+    datalist = datastr.replace(',','').lower().split('e')
+    return np.float64(datalist[0])*10**int(datalist[1])

mechanics/body.py
@@ -9,17 +9,18 @@ from mechanics.centres import *
 
 class Body(Entity):
     def __init__(self, data, universe, focus):
-        self.bodycentre, self.barycentre = create_centres(self, sn(data[10]), universe.grav_constant)
+        self.bodycentre, self.barycentre = create_centres(self, data, universe.grav_constant)
         super().__init__(data, universe, focus)
         mass_scale = 0.0001*np.log10(self.bodycentre.mass)
         self.spheroid = Spheroid(self)
 
         if self.primary:
             self.primary.satellites.append(self)
-            self.primary.object.barycentre.SGP += self.barycentre.SGP
-            self.primary.object.barycentre.mass += self.barycentre.mass
-            self.primary.object.barycentre.inverse_SGP = 1/self.primary.object.barycentre.SGP
-            self.primary.object.barycentre.inverse_mass = 1/self.primary.object.barycentre.mass
+            if not self.primary.object.barycentre.complete:
+                self.primary.object.barycentre.SGP += self.barycentre.SGP
+                self.primary.object.barycentre.mass += self.barycentre.mass
+                self.primary.object.barycentre.inverse_SGP = 1/self.primary.object.barycentre.SGP
+                self.primary.object.barycentre.inverse_mass = 1/self.primary.object.barycentre.mass
             self.trace = Trace(self, 52, True)
             self.label = EntityLabel(self, width = mass_scale, height = 20*mass_scale)
         else:
@@ -36,9 +37,9 @@ class Body(Entity):
         universe.bodies.append(self)
 
     def shape_and_orientation(self, data):
-        self.rotation_rate = float(data[14])/86400
-        self.shift = float(data[13]) + float(data[15]) - 946684800.0*self.rotation_rate
-        self.tilt_quaternion, self.axis_vector, self.ECL_to_EQU_matrix, self.EQU_to_ECL_matrix = celestial_to_ecliptic(float(data[11]),float(data[12]))
+        self.rotation_rate = float(data[13])/86400
+        self.shift = float(data[12]) + float(data[14]) - 946684800.0*self.rotation_rate
+        self.tilt_quaternion, self.axis_vector, self.ECL_to_EQU_matrix, self.EQU_to_ECL_matrix = celestial_to_ecliptic(float(data[10]),float(data[11]))
 
         self.oblateness = (self.radii[0]-self.radii[2])/self.radii[0]
         self.precession_constant = -(self.radii[0]**2)*(2*self.oblateness-(self.radii[0]**3)*(np.deg2rad(self.rotation_rate)**2)*self.bodycentre.inverse_SGP)/2

mechanics/centres.py
@@ -1,8 +1,25 @@
 import numpy as np
 
-def create_centres(object, mass, grav_constant):
-    SGP = mass*grav_constant
-    return Bodycentre(object, mass, SGP), Barycentre(object, mass, SGP)
+def sn(datastr):
+    datalist = datastr.replace(',','').lower().split('e')
+    return np.float64(datalist[0])*10**int(datalist[1])
+
+def create_centres(object, data, grav_constant):
+    if data[8] == 'Mass':
+        mass = sn(data[9])
+        SGP = mass*grav_constant
+        return Bodycentre(object, mass, SGP), Barycentre(object, mass, SGP, complete=False)
+
+    elif data[8] == 'False':
+        SGP = np.float64(data[9])*1e9
+        mass = SGP/grav_constant
+        return Bodycentre(object, mass, SGP), Barycentre(object, mass, SGP, complete=False)
+    else:
+        bary_SGP = np.float64(data[8])*1e9
+        body_SGP = np.float64(data[9])*1e9
+        bary_mass = bary_SGP/grav_constant
+        body_mass = body_SGP/grav_constant
+        return Bodycentre(object, body_mass, body_SGP), Barycentre(object, bary_mass, bary_SGP, complete=True)
 
 class Centre():
     def __init__(self, object, mass, SGP):
@@ -15,20 +32,14 @@ class Centre():
         self.SGP = SGP
         self.inverse_SGP = 1/SGP
 
-    def sibling_collisions(self, universe, vessel):
-        for sibling in self.object.satellites:
-            if False:
-            #if pvdis < sibling.SOI:
-                entity.change_primary(universe, sibling, barycentre=True)
-
 class Barycentre(Centre):
-    def __init__(self, object, mass, SGP):
+    def __init__(self, object, mass, SGP, complete=False):
         super().__init__(object, mass, SGP)
+        self.complete=complete
         self.ppos = np.array([0.,0.,0.],dtype=np.float64)
         self.pvel = np.array([0.,0.,0.],dtype=np.float64)
 
     def check_model(self, universe, vessel):
-        self.sibling_collisions(universe, vessel)
         if vessel.orbit.periapsis < self.object.barycentre_transition:
             vessel.change_primary(universe, vessel.primary.object, barycentre=False)
         elif vessel.orbit.rel_dist > vessel.primary.object.SOI:
@@ -41,7 +52,6 @@ class Bodycentre(Centre):
         super().__init__(object ,mass, SGP)
 
     def check_model(self, universe, vessel):
-        self.sibling_collisions(universe, vessel)
         if vessel.orbit.periapsis > self.object.barycentre_transition:
             vessel.change_primary(universe, vessel.primary.object, barycentre=True)
         elif vessel.orbit.rel_dist < vessel.primary.object.mean_radius:

mechanics/entity.py
@@ -2,21 +2,20 @@ import numpy as np
 import statistics
 import time
 
-from data.initial_conditions.read_conditions import *
 from mechanics.orbit import *
 from graphics.labels import *
 from graphics.trace import *
 from mechanics.centres import *
 
 class Entity():
-    def __init__(self, data, universe, focus):
+    def __init__(self, data, universe, focus, radii=True):
         self.name = data[0]
         self.primary = False
-        self.radii = 1000*np.array([float(data[7]),float(data[8]),float(data[9])])
+        self.radii = 1000*np.array([float(data[5]),float(data[6]),float(data[7])] if radii else [0.01,0.01,0.01])
         self.mean_radius = statistics.mean(self.radii)
         universe.entities.append(self)
         self.focus_num = len(universe.entities)-1
-        self.color = np.array([float(i) for i in (data[4].split(','))])
+        self.color = np.array([float(i) for i in (data[3].split(','))])
         self.colorsmall = self.color/255
         self.specific_strength = 1
         self.orbit = Orbit(self)
@@ -26,8 +25,11 @@ class Entity():
             universe.focus_entity = self
 
         for body in universe.bodies:
-            if body.name == data[2]:
-                self.primary = body.barycentre if data[3] == 'barycentre' else body.bodycentre
-                self.barycentre_model = data[3] == 'barycentre'
-                read_initial_state(self, data, universe) if data[5] == 'state' else read_initial_elements(self, data, universe)
+            if body.name == data[1]:
+                self.primary = body.barycentre if data[2] == 'bary' else body.bodycentre
+                self.barycentre_model = data[2] == 'bary'
+                if data[4] == 'elements':
+                    universe.read_initial_elements(self)
+                elif data[4] == 'TLE':
+                    universe.read_initial_TLE(self)
                 self.local_planet = self.primary.object.local_planet if self.primary.object.primary else self

mechanics/pertubations.py
@@ -9,26 +9,26 @@ def perturbations(universe):
     for entity in universe.entities[1:]:
         if universe.timestep < 0.02*entity.orbit.period:
 
-            ''' Apply pertubations from its grandparent, if it has one. E.g. sun to moon.'''
-            if entity.primary.object.primary:
-                grandparent_vec = entity.primary.object.barycentre.rpos + entity.barycentre.rpos #THIS IS INCORRECT FOR BODYCENTRE ORBITS. Check vector algebra taking into account parent bodycentre around parent barycentre.
-                entity.barycentre.pvel += universe.timestep*entity.primary.object.primary.SGP*((-grandparent_vec/np.linalg.norm(grandparent_vec)**3) - (-entity.primary.object.barycentre.rpos/np.linalg.norm(entity.primary.object.barycentre.rpos)**3))
-
-            ''' Include a term for notable piblings '''
-
-            ''' Correct for parent error '''
+            ''' 1. Correct for parent error '''
             #if entity.isbarycentric:
             if isinstance(entity.primary, Barycentre) and isinstance(entity, Vessel):
                 true_vec = entity.primary.object.bodycentre.rpos-entity.barycentre.rpos
                 entity.barycentre.pvel += universe.timestep*entity.primary.object.bodycentre.SGP*((true_vec/np.linalg.norm(true_vec)**3) + (entity.barycentre.rpos/np.linalg.norm(entity.barycentre.rpos)**3))
 
-            ''' Apply pertubations from relevant siblings (bodies orbiting around the same parent) '''
-            '''
-            for sibling in entity.major_siblings:
-                pvvec = 0.
-                psvec = 0.
-                entity.barycentre.pvel += universe.timestep*sibling.barycentre.SGP*(0)
-            '''
+            ''' 2. Gravity Harmonics '''
+
+            ''' 3. Pertubations from notable siblings '''
+
+            ''' 4. Pertubations from grandparent '''
+            if entity.primary.object.primary:
+                grandparent_vec = entity.primary.object.barycentre.rpos + entity.barycentre.rpos #THIS IS INCORRECT FOR BODYCENTRE ORBITS. Check vector algebra taking into account parent bodycentre around parent barycentre.
+                entity.barycentre.pvel += universe.timestep*entity.primary.object.primary.SGP*((-grandparent_vec/np.linalg.norm(grandparent_vec)**3) - (-entity.primary.object.barycentre.rpos/np.linalg.norm(entity.primary.object.barycentre.rpos)**3))
+
+            ''' 5. Pertubations from notable piblings '''
+
+            ''' 6. Pertubations from notable niblings '''
+
+            ''' 7. General Relativity '''
 
             ''' Update true positions due to pertubations '''
             entity.barycentre.ppos += entity.barycentre.pvel*universe.timestep